856f9d49c540c7325f89d0079a23d4a5fcc621de
Tianon Gravi <admwiggin@gmail.com>
2016-04-15 10:16:43 -0700

    Better output from our "application"

--- hal-9000/app.sh
+++ hal-9000/app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if ! echo 'Open the pod bay doors, HAL.' | tee -a /var/lib/app/instructions.txt; then
+if ! echo '< Open the pod bay doors, HAL.' | tee -a /var/lib/app/instructions.txt 2>/dev/null; then
 	cat <<-EOF
-		I'm sorry Dave, I'm afraid I can't do that.
+		> I'm sorry Dave, I'm afraid I can't do that.
 	EOF
 else
 	cat <<-EOF
-		Daisy, Daisy, give me your aaannnssswwweeerrr ddddoooo......
+		> Daisy, Daisy, give me your aaannnssswwweeerrr ddddoooo......
 	EOF
 fi
