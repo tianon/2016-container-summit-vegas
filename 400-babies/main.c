dc39b19e3802c41bbaa7bedcf976f4221687384f
Tianon Gravi <admwiggin@gmail.com>
2016-04-15 04:53:33 -0700

    Pretend we're clever with some witty comments

--- 400-babies/main.c
+++ 400-babies/main.c
@@ -14,6 +14,10 @@ int main() {
 	//if (signal(SIGTERM, sighdlr) == SIG_ERR) { perror(0); exit(1); }
 	//if (signal(SIGINT, sighdlr) == SIG_ERR) { perror(0); exit(1); }
 
+	// not quite a "forkbomb", but maybe a "forkcandle" ?
+	//   doesn't cause immediate damage,
+	//   but if you leave it going too long,
+	//   it might cause problems over time
 	int pid;
 	while ((pid = fork()) != 0) {
 		printf("POWERTHIRST: %d\n", pid);
