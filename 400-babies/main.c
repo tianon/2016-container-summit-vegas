35fed090268b96894c7297fa316e3ff73eb2c93c
Tianon Gravi <admwiggin@gmail.com>
2016-04-15 04:44:27 -0700

    Add more boilerplate

--- 400-babies/main.c
+++ 400-babies/main.c
@@ -1,10 +1,24 @@
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/** /
+void sighdlr(int sig) {
+	printf("lolsig %d\n", sig);
+	exit(0);
+}
+/**/
 
 int main() {
+	//if (signal(SIGCHLD, SIG_IGN) == SIG_ERR) { perror(0); exit(1); }
+	//if (signal(SIGTERM, sighdlr) == SIG_ERR) { perror(0); exit(1); }
+	//if (signal(SIGINT, sighdlr) == SIG_ERR) { perror(0); exit(1); }
+
 	int pid;
 	while ((pid = fork()) != 0) {
 		printf("POWERTHIRST: %d\n", pid);
 		sleep(1);
 	}
+
 	return 0;
 }
