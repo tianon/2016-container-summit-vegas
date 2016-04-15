b91ffdab3bbee17cfb22eb8a9d2ec24e9cf7215f
Tianon Gravi <admwiggin@gmail.com>
2016-04-15 10:04:21 -0700

    Add more boilerplate

--- hal-9000/app.sh
+++ hal-9000/app.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
-cat <<-EOF
-	I'm sorry Dave, I'm afraid I can't do that.
-EOF
+if ! echo 'Open the pod bay doors, HAL.' | tee -a /var/lib/app/instructions.txt; then
+	cat <<-EOF
+		I'm sorry Dave, I'm afraid I can't do that.
+	EOF
+else
+	cat <<-EOF
+		Daisy, Daisy, give me your aaannnssswwweeerrr ddddoooo......
+	EOF
+fi
