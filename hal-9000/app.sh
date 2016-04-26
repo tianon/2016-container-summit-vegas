fd21ec178fc91f509d7d9a6b183f97d04a2700e3
Tianon Gravi <admwiggin@gmail.com>
2016-04-26 15:44:02 -0700

    Prep for running "app.sh" as non-root

--- hal-9000/app.sh
+++ hal-9000/app.sh
@@ -3,9 +3,15 @@ set -e
 
 if ! echo '< Open the pod bay doors, HAL.' | tee -a /var/lib/app/instructions.txt 2>/dev/null; then
 	cat <<-EOF
-		> I'm sorry Dave, I'm afraid I can't do that.
+		> I'm sorry $(id -un), I'm afraid I can't do that.
 	EOF
 else
+	if [ "$(id -u)" = 0 ]; then
+		echo >&2
+		echo >&2 "warning: running $0 as root is not recommended /o\\"
+		echo >&2
+	fi
+
 	cat <<-EOF
 		> Daisy, Daisy, give me your aaannnssswwweeerrr ddddoooo......
 	EOF
